202e6cbba6df8eb75569ccc97cd359cd7507ed3c
Christophe de BORTOLI <cdebortoli@sqli.com>
2013-06-05 22:56:19 +0200

    SpriteBatch / SpriteCache  and images

--- test1screen/test1Screen/src/com/me/test1screen/screens/gameScreen.java
+++ test1screen/test1Screen/src/com/me/test1screen/screens/gameScreen.java
@@ -4,25 +4,72 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.SpriteCache;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 import com.me.test1screen.test1Screen;
 
 public class gameScreen extends abstractScreen {
 
+	// Camera & shapes
 	private OrthographicCamera cam;
     ShapeRenderer debugRenderer = new ShapeRenderer();
     float rotationSpeed = 0.5f;
 
-    static final int VIEWPORT_WIDTH_UNITS  = 11;
-    static final int VIEWPORT_HEIGHT_UNITS = 11;
+    // Size values
+    static final int VIEWPORT_WIDTH_UNITS  = 10;
+    static final int VIEWPORT_HEIGHT_UNITS = 10;
 
-    static final int RECT_WIDTH_UNITS  = 50;
-    static final int RECT_HEIGHT_UNITS = 50;
+    static final int RECT_WIDTH_UNITS  = 2000;
+    static final int RECT_HEIGHT_UNITS = 2000;
     
+    // Used for images
+    private TextureRegion[] squareTextures = new TextureRegion[5];
+    private SpriteBatch spriteBatch;
+    private SpriteCache spriteCache;
+    private int _spriteCacheIndex;  
+    private int[][] colorIndexArray;
     
     public gameScreen(test1Screen game) {
 		super(game);
+		
+        spriteBatch = new SpriteBatch();
+        spriteCache = new SpriteCache(RECT_WIDTH_UNITS*RECT_HEIGHT_UNITS, false);
+        
+        // Create the random quadrillage
+        colorIndexArray = new int[RECT_WIDTH_UNITS][RECT_HEIGHT_UNITS];
+		int i_color = 0;
+        for (int i = -RECT_WIDTH_UNITS/2; i < RECT_WIDTH_UNITS/2; i++) {
+			int j_color = 0;
+        	for (int j = -RECT_HEIGHT_UNITS/2; j < RECT_HEIGHT_UNITS/2; j++) {
+				int random = (int)(Math.random() * (5-1)) + 1;
+				colorIndexArray[i_color][j_color] = random;
+				j_color++;
+        	}
+        	i_color++;
+		}
+		
+		// Load images
+        TextureAtlas atlas = new TextureAtlas(Gdx.files.internal("data/textures/textures.pack"));
+        for (int i = 1; i <= 5; i++) {
+			squareTextures[i-1] = atlas.findRegion("square"+String.valueOf(i));
+		}
+        
+        // Create the sprite cache
+        spriteCache.beginCache();
+		int sprite_i_color = 0;
+		for (int i = -RECT_WIDTH_UNITS/2; i < RECT_WIDTH_UNITS/2; i++) {
+			int sprite_j_color = 0;
+			for (int j = -RECT_HEIGHT_UNITS/2; j < RECT_HEIGHT_UNITS/2; j++) {
+				spriteCache.add(squareTextures[colorIndexArray[sprite_i_color][sprite_j_color]], i, j, 1, 1);
+				sprite_j_color++;
+			}
+			sprite_i_color++;
+		}
+		_spriteCacheIndex = spriteCache.endCache();
 	}
     
 	@Override
@@ -30,20 +77,48 @@ public class gameScreen extends abstractScreen {
 
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		Gdx.gl.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-		
+
+		// Handle input of the camera
 		handleInput();
 
+		// Set camera
+		debugRenderer.setProjectionMatrix(cam.combined);
+		spriteBatch.setProjectionMatrix(cam.combined);
+	    spriteCache.setProjectionMatrix(cam.combined);
+		
+		// Sprite batch
+//		spriteBatch.begin();
+//			int i_color = 0;
+//			for (int i = -RECT_WIDTH_UNITS/2; i < RECT_WIDTH_UNITS/2; i++) {
+//				int j_color = 0;
+//				for (int j = -RECT_HEIGHT_UNITS/2; j < RECT_HEIGHT_UNITS/2; j++) {
+//					spriteBatch.draw(squareTextures[colorIndexArray[i_color][j_color]], i, j, 1, 1);
+//					j_color++;
+//				}
+//				i_color++;
+//			}
+//		
+//		spriteBatch.end();
+		
+		// Sprite cache;    
+        spriteCache.begin();  
+        spriteCache.draw(_spriteCacheIndex);  
+        spriteCache.end();  
+		
+		
+		// Draw big square
 		debugRenderer.begin(ShapeType.Rectangle);
 		debugRenderer.rect( -RECT_WIDTH_UNITS/2, -RECT_HEIGHT_UNITS/2, RECT_WIDTH_UNITS, RECT_HEIGHT_UNITS);
 		
+		// Draw quadrillage
 		for (int i = -RECT_WIDTH_UNITS/2; i < RECT_WIDTH_UNITS/2; i++) {
 			for (int j = -RECT_HEIGHT_UNITS/2; j < RECT_HEIGHT_UNITS/2; j++) {
-				debugRenderer.rect(i,j,1,1);
+				//debugRenderer.rect(i,j,1,1);
 			}
-			
 		}
-		debugRenderer.setProjectionMatrix(cam.combined);
 		debugRenderer.end();
+		
+		
 		
 		cam.update();
 		//cam.apply(Gdx.gl10);
@@ -89,10 +164,11 @@ public class gameScreen extends abstractScreen {
 	
     private void handleInput() {
         if(Gdx.input.isKeyPressed(Input.Keys.A)) {
-                cam.zoom += 0.02;
-        }
+        	if(cam.zoom < 1000)
+                cam.zoom += 0.5;        }
         if(Gdx.input.isKeyPressed(Input.Keys.Q)) {
-                cam.zoom -= 0.02;
+        	if(cam.zoom > 0.5)
+                cam.zoom -= 0.5;
         }
         if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
                 if (cam.position.x > (-RECT_WIDTH_UNITS / 2) - 5)
---
+++ test1screen/test1Screen/src/com/me/test1screen/utils/TextureSetup.java
@@ -0,0 +1,11 @@
+package com.me.test1screen.utils;
+
+import com.badlogic.gdx.tools.imagepacker.TexturePacker2;
+
+public class TextureSetup {
+
+	public static void main(String[] args) {
+		TexturePacker2.process("/utils/libgdx/myWork/libgdx1_test1_screen_camera/test1screen/test1Screen-android/assets/data", "/utils/libgdx/myWork/libgdx1_test1_screen_camera/test1screen/test1Screen-android/assets/data/textures", "textures.pack");
+	}
+}
+
